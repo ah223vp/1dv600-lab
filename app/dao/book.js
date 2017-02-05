9ae6695520f01bc0a5b403cddc215ebf5908b24f
Andreas Hedin <ah223vp@student.lnu.se>
2017-02-05 10:16:11 +0100

    Assignment 1 doneÃ

--- app/dao/book.js
+++ app/dao/book.js
@@ -2,6 +2,20 @@
  * Created by Andreas on 2017-01-19.
  */
 
+/**
+ * Creates and returns a book object to the user. No Error handling
+ * is yet to be made.
+ * @param id of the book.
+ * @param title of the book.
+ * @param author of the book.
+ * @param genre the book belongs to.
+ * @param publishDate when the books was published.
+ * @param decription short decription of the books plot.
+ * @param price Price of the book.
+ * return statements returns a book object contaning the above data.
+ * @returns {{id: *, title: *, author: *, genre: *, publishDate: *, description: *, price: *}}
+ */
+
 function book(id, title, author, genre, publishDate, decription, price){
 
 return {
--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -2,21 +2,29 @@
     "use strict";
 
     var LibraryDAO = require('../dao/LibraryDAO');
+    // Book object. Construct with new.
     let Book = require("../dao/book.js");
-    // Create books somewhere else, just for testing
-    let book1 = new Book("1", "Title", "Author", "Genre", "PublishDate", "Description", "0");
-    let book2 = new Book("2", "Hitchhikers Guide to The Galaxy", "Douglas Adams", "Fantasy/Sci-fi",
-        "1995-09", "What is the meaning to life universe and everything?", "199");
-    let book3 = new Book("3", "Title", "Author", "Genre", "PublishDate", "Description", "0");
-    let book4 = new Book("4", "Title", "Author", "Genre", "PublishDate", "Description", "0");
-    let book5 = new Book("5", "Title", "Author", "Genre", "PublishDate", "Description", "0");
-
-    let parse = JSON.stringify([book1,book2,book3,book4,book5]);
-
 
 
     module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
-        callback(parse);
+        /**
+         * Creating the list directly in the callback, assuming this will be made
+         * nicer later on.
+         * There is no problem to move the list later on and call it from
+         * somewhere else.
+         * Converting the array to JSON directly aswell.
+         */
+        callback(JSON.stringify(
+            [
+                new Book("1", "Title", "Author", "Genre", "PublishDate", "Description", "0"),
+                new Book("2", "Hitchhikers Guide to The Galaxy", "Douglas Adams", "Fantasy/Sci-fi",
+                    "1995-09", "What is the meaning of life universe and everything?", "199"),
+                new Book("3", "Title", "Author", "Genre", "PublishDate", "Description", "0"),
+                new Book("4", "Title", "Author", "Genre", "PublishDate", "Description", "0"),
+                new Book("5", "Title", "Author", "Genre", "PublishDate", "Description", "0")
+
+            ]
+        ))
     };
 
 }());
